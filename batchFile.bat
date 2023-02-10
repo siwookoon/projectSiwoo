58b82aa482a6dfca65400e76edba2ae7d6176ba1
siwookoon <https://github.com/siwookoon>
2023-02-10 12:46:10 +0900

    updated from batch file

--- batchFile.bat
+++ batchFile.bat
@@ -1,4 +1,4 @@
-call C:\Users\h\Desktop\projectSiwoo\venv\Scripts\activate
-@cd C:\Users\h\Desktop\projectSiwoo
+call C:\Users\siwoo537\Desktop\projectSiwoo\venv\Scripts\activate
+@cd C:\Users\siwoo537\Desktop\projectSiwoo
 python update.py
 pause
--- submit.bat
+++ submit.bat
@@ -1,4 +1,4 @@
-set root=C:\Users\human\Desktop\ProjectSiwoo
+set root=C:\Users\siwoo537\Desktop\projectSiwoo
 call git add .
 call git commit -m "updated from batch file"
 call git push
